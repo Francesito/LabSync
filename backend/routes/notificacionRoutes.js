72f989e7b57488ed44a786da017ee27075ed64ac
Francesito <103217923+Francesito@users.noreply.github.com>
2025-08-16 01:12:47 -0600

    Update notificacionRoutes.js

--- backend/routes/notificacionRoutes.js
+++ backend/routes/notificacionRoutes.js
@@ -4,6 +4,7 @@ const { verificarToken } = require('../middleware/authMiddleware');
 const notificacionController = require('../controllers/notificacionController');
 
 router.get('/', verificarToken, notificacionController.obtenerNotificaciones);
+router.delete('/', verificarToken, notificacionController.eliminarTodas);
 router.delete('/:id', verificarToken, notificacionController.eliminar);
 
 module.exports = router;
