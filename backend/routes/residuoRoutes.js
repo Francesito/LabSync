82f0f639134c56732fe50de4bae27dacfd0ff43e
Francesito <103217923+Francesito@users.noreply.github.com>
2025-08-15 20:16:41 -0600

    Update residuoRoutes.js

--- backend/routes/residuoRoutes.js
+++ backend/routes/residuoRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerResiduos, registrarResiduo } = require('../controllers/residuoController');
+const { obtenerResiduos, registrarResiduo, eliminarResiduos } = require('../controllers/residuoController');
 
 router.get('/', obtenerResiduos);
 router.post('/', registrarResiduo);
+router.delete('/', eliminarResiduos);
 
 module.exports = router;
