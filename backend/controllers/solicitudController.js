2439e06e8de299cf3d3fa66edda4d14f4ac15cee
Francesito <103217923+Francesito@users.noreply.github.com>
2025-08-18 19:44:59 -0600

    Update solicitudController.js

--- backend/controllers/solicitudController.js
+++ backend/controllers/solicitudController.js
@@ -479,10 +479,7 @@ const entregarMateriales = async (req, res) => {
     }
 
    const reco = solicitud[0].fecha_recoleccion;
-    const fmt = (d) =>
-      new Date(d.getTime() - d.getTimezoneOffset() * 60000)
-        .toISOString()
-        .split('T')[0];
+const fmt = (d) => new Date(d).toISOString().split('T')[0];
     if (!reco || fmt(reco) !== fmt(new Date())) {
       await connection.rollback();
       return res
