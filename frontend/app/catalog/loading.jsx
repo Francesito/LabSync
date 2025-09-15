720611fb9f81a78db792abd50445b16f0020977d
Francesito <103217923+Francesito@users.noreply.github.com>
2025-09-14 23:42:22 -0600

    Add files via upload

---
+++ frontend/app/catalog/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-vh-100 d-flex justify-content-center align-items-center bg-white">
+      <p className="text-primary">Espere un segundo...</p>
+    </div>
+  );
+}
